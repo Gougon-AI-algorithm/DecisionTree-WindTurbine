5d51666fe9164f7f201c83ffa5c501d24ae5bb0f
gougon <freddy2531@gmail.com>
2019-12-19 14:21:58 +0800

    Implement make_decision_tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,36 +1,31 @@
 import math
 import Constant
-import DFHelper
+import Node
 
 
 class DecisionTree:
-    def __init__(self, df_helper):
-        self.df_helper = df_helper
-        self._raise_df_helper_type()
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
 
-        self.df_helper.choose_necessary_data(Constant.ATTRS_NAME)
-        self.df_helper.drop_na()
+        self.all_labels = self._get_labels()
+        self.p_conditions = Constant.P_NODES_INTERVAL
 
-        self.attrs = [df_helper.get_column(Constant.WS_ATTR_NAME), df_helper.get_column(Constant.RM_ATTR_NAME),
-                      df_helper.get_column(Constant.CM_ATTR_NAME), df_helper.get_column(Constant.RS_ATTR_NAME)]
-        self.attrs_labels = self._get_attrs_labels()
-        self.p = df_helper.get_column(Constant.P_ATTR_NAME)
-        self.p_labels = Constant.P_NODES_INTERVAL
-        self._make_decision_tree()
+        self.root = self._make_decision_tree(self.dataframe, self.all_labels)
+        print(self.root)
 
-    def _raise_df_helper_type(self):
-        if not isinstance(self.df_helper, DFHelper.DFHelper):
-            raise TypeError(Constant.DF_HELPER_TYPE_ERROR)
-
-    def _get_attrs_labels(self):
+    def _get_labels(self):
         nodes = {}
-        for attr_order in range(len(self.attrs)):
-            attr_labels = self._get_attr_labels(attr_order)
+        attrs = [self.dataframe.get_column(Constant.WS_ATTR_NAME), self.dataframe.get_column(Constant.RM_ATTR_NAME),
+                 self.dataframe.get_column(Constant.CM_ATTR_NAME), self.dataframe.get_column(Constant.RS_ATTR_NAME)]
+        attr_order = 0
+        for attr in attrs:
+            attr_labels = self._get_attr_labels(attr)
             nodes[Constant.ATTRS_NAME[attr_order]] = attr_labels
+            attr_order += 1
         return nodes
 
-    def _get_attr_labels(self, attr_order):
-        attr = self.attrs[attr_order]
+    def _get_attr_labels(self, attr):
+        self._do_nothing()
         min_val = min(attr)
         max_val = max(attr)
         interval = (max_val - min_val) / Constant.CLUSTER_NUMBER
@@ -40,14 +35,34 @@ class DecisionTree:
             attr_labels.append(cut_point)
         return attr_labels
 
-    def _make_decision_tree(self):
-        entropy = self._calc_entropy(self.p, self.p_labels)
-        information_gain = 0
+    def _make_decision_tree(self, data_set, labels):
+        node = Node.Node()
+        label = self._get_highest_information_gain_label(data_set, labels)
+        node.set_value(label)
+        children = []
+        for condition_count in range(len(self.all_labels[label])):
+            data_set = self._split_data_set(data_set, label, condition_count)
+            children.append(self._make_decision_tree(data_set))
+        node.set_children(children)
+        return node
 
-    def _calc_entropy(self, data_set, labels):
+    def _get_highest_information_gain_label(self, data_set, labels):
+        label_entropy = {}
+        for label in labels:
+            conditions = labels[label]
+            label_entropy[label] = self._calc_entropy(data_set[label], conditions)
+        return max(label_entropy, key=label_entropy.get)
+
+    def _split_data_set(self, data_set, label, condition_count):
+        left_margin = self.all_labels[label][condition_count]
+        right_margin = self.all_labels[label][condition_count + 1]
+        data_set = data_set[left_margin <= data_set[label] < right_margin]
+        return data_set
+
+    def _calc_entropy(self, data_set, conditions):
         label_counts = {}
         for data in data_set:
-            self._add_data_into_label_counts(data, label_counts, labels)
+            self._add_data_into_label_counts(data, label_counts, conditions)
         entropy = 0
         for label_count in label_counts.values():
             prob = label_count / len(data_set)
@@ -64,8 +79,8 @@ class DecisionTree:
                     label_counts.setdefault(left_margin, 0)
                 label_counts[left_margin] += 1
 
-    def get_attrs_labels(self):
-        return self.attrs_labels
+    def get_labels(self):
+        return self.all_labels
 
     def _do_nothing(self):
         pass
